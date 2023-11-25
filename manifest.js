70d80d2b4539b52dc48281dc6d9d000f1937c532
Pandu Supriyono <mail@supriyono.nl>
2023-11-25 14:32:19 +0100

    feat: rm storages

--- manifest.js
+++ manifest.js
@@ -9,7 +9,7 @@ const manifest = {
   name: packageJson.name,
   version: packageJson.version,
   description: packageJson.description,
-  permissions: ['storage', 'activeTab', 'scripting'],
+  permissions: ['activeTab', 'scripting'],
   options_page: 'src/pages/options/index.html',
   background: {
     service_worker: 'src/pages/background/index.js',
